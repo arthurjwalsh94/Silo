2cf1189b5085cb15f4d618760f982021e935d11c
Arthur Walsh <arthurwalsh@Arthurs-MBP-2.lan>
2025-01-03 21:10:36 -0500

    Add connection to Docker-based Postgres with TypeORM in app.module.ts

--- apps/Backend/src/app/app.module.ts
+++ apps/Backend/src/app/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'mySecretPass',
+      database: 'silo_database',
+      autoLoadEntities: true,
+      synchronize: true, // For development only
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
